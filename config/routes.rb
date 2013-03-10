e4485e0487885364c8ba687f1f7196fca4b5252b
Arda Ayvaz <ardaayvaz@gmail.com>
2013-03-10 15:43:46 -0700

    tekrardan

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/home"
 
-  get "static_pages/help"
+  get "static_pages/help" #bir problem var burda ama cozemedim
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
